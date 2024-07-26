da7db9dcafd33b311cc2ede98d757b52de2b5098
João Pedro Thomaz <jojopepedro@hotmail.com>
2024-07-25 21:57:07 -0300

    Classe pai (CONTA)

---
+++ class_conta.js
@@ -0,0 +1,8 @@
+class Conta{
+    constructor(titular, saldo, agencia, data_criacao, ){
+        this.titular = titular,
+        this.saldo = saldo,
+        this.agencia = agencia,
+        this.data_criacao = data_criacao;
+    }
+}
