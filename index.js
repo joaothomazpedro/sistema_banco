0d2dc50a4d267caf4fe349d44eb3af7e376425b7
João Pedro Thomaz <jojopepedro@hotmail.com>
2024-07-25 23:56:00 -0300

    Pequenas mudanças

--- index.js
+++ index.js
@@ -5,28 +5,30 @@ const Conta_Corrente = require("./class_conta_corrente");
 const Conta_Poupanca = require("./class_conta_poupanca");
 
 /* prpt = prompt abreviado*/
+while (true) {
+    prpt_conta = prompt("Olá, deseja criar uma conta?: [Y/N] ").toLowerCase()
+    if (prpt_conta == "y") {
 
-prpt_conta = prompt("Olá, deseja criar uma conta?: [Y/N] ").toLowerCase()
-if (prpt_conta == "y") {
-    
-    prpt_name = prompt("Excelente, qual seria seu nome?: ");
-    
-    if (isNaN(prpt_name)) {
-        Count = new Conta(prpt_name,1,0)
-        console.log(Count);        
-    } 
+        prpt_name = prompt("Excelente, qual seria seu nome?: ");
+
+        if (isNaN(prpt_name)) {
+            Count = new Conta(prpt_name, 1, 0)
+            console.log(Count);
+        }
+
+        else {
+            console.log("Digite um nome válido por favor");
+        }
+    }
+
+    else if (prpt_conta == "n") {
+        console.log("Até outro dia");
+        break
+    }
 
     else {
-        console.log("Digite um nome válido por favor");
-    }}
-
-    else if(prpt_conta == "n") {
-        console.log("Até outro dia");
-    }
-    
-    else{
         console.log("Por favor confirme novamente: [Y/N]");
     }
-
+}
 
 
