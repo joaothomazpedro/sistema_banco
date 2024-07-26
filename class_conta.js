85ee315130a85b86e83e716e3725f3c3f89772a9
João Pedro Thomaz <jojopepedro@hotmail.com>
2024-07-25 23:51:30 -0300

    Criação de Conta Base

--- class_conta.js
+++ class_conta.js
@@ -1,11 +1,40 @@
-class Conta{
-    constructor(titular, saldo, agencia, data_criacao, ){
+class Conta {
+    constructor(titular, id_conta, saldo, data_criacao, transacoes) {
         this.titular = titular,
-        this.saldo = saldo,
-        this.agencia = agencia,
-        this.data_criacao = data_criacao;
+            this.id_conta = id_conta,
+            this.saldo = saldo,            
+            this.data_criacao = new Date(),
+            this.transacoes = [];
+    }
+    
+    depositar(valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            this.transacoes.push(`Depósito: ${valor}R$`)
+            console.log(`Depósito de ${valor} realizada`);
+        }
+        else {
+            console.log("Valor de depósito inválido tente novamente");
+        }
+    }
+
+    sacar(valor) {
+        if (0 < valor <= this.saldo) {
+            this.saldo -= valor;
+            this.transacoes.push(`Saque: ${valor}R$`)
+            console.log(`Saque de ${valor} realizada`);
+        }
+        else {
+            console.log("Valor de saque inválido tente novamente");
+        }
+    }
+
+    exibir_saldo(){
+        console.log(`Saldo atual de: ${this.saldo} `);
     }
 }
+
+
 
 
 module.exports = Conta;
--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const Conta = require("./class_conta");
 const Conta_Corrente = require("./class_conta_corrente");
 const Conta_Poupanca = require("./class_conta_poupanca");
 
+/* prpt = prompt abreviado*/
 
+prpt_conta = prompt("Olá, deseja criar uma conta?: [Y/N] ").toLowerCase()
+if (prpt_conta == "y") {
+    
+    prpt_name = prompt("Excelente, qual seria seu nome?: ");
+    
+    if (isNaN(prpt_name)) {
+        Count = new Conta(prpt_name,1,0)
+        console.log(Count);        
+    } 
 
+    else {
+        console.log("Digite um nome válido por favor");
+    }}
+
+    else if(prpt_conta == "n") {
+        console.log("Até outro dia");
+    }
+    
+    else{
+        console.log("Por favor confirme novamente: [Y/N]");
+    }
 
 
 
